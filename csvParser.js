8b926273ce05872f635089feca4aef06115f136c
Shailla <107023318+shai-mohan@users.noreply.github.com>
2024-11-06 00:41:51 +0800

    completed assessment (html and js files)

---
+++ csvParser.js
@@ -0,0 +1,12 @@
+function parseCSV(text) {
+    const lines = text.trim().split('\n');
+    const headers = lines[0].split(',');
+    return lines.slice(1).map(line => {
+      const values = line.split(',');
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header.trim()] = values[index].trim();
+      });
+      return obj;
+    });
+}  
